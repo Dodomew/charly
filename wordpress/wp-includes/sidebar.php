f1682585c8004f14f3d528046539148bd47d201c
Doortje Spanjerberg <dodomew@gmail.com>
2016-05-25 19:28:52 +0200

    Wordpress ready gemaakt

---
+++ wordpress/wp-includes/sidebar.php
@@ -0,0 +1,10 @@
+    <div class="menu-container">
+        
+        <ul class="menu-list">
+            
+            <li><a href="">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact me</a></li>
+            
+        </ul>
+
