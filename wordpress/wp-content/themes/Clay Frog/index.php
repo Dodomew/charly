01548405f85af7cfa95d4cc5303d7a7620eb3969
Doortje Spanjerberg <dodomew@gmail.com>
2016-06-07 20:23:01 +0200

    Wordpress fixed and gitignore file added

--- wordpress/wp-content/themes/Clay Frog/index.php
+++ wordpress/wp-content/themes/Clay Frog/index.php
@@ -5,39 +5,45 @@
     <?php if ( have_posts() ) : ?>
 
         <?php while ( have_posts() ) : the_post(); ?>
+		
+			<?php if ( is_home() ) : ?>
+		
+				<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a>
+		
+			<?php else : ?>
 
-            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                <div class="post-header">
+					<div class="post-header">
 
-                    <div class="date"><?php the_time( 'M j y' ); ?></div>
+						<div class="date"><?php the_time( 'M j y' ); ?></div>
 
-                    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-                    <div class="author"><?php the_author(); ?></div>
+						<div class="author"><?php the_author(); ?></div>
 
-                </div><!--.post-header-->
+					</div><!--.post-header-->
 
-                <div class="entry clear">
+					<div class="entry clear">
 
-                    <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+						<?php the_content(); ?>
 
-                    <?php the_content(); ?>
+						<?php edit_post_link(); ?>
 
-                    <?php edit_post_link(); ?>
+						<?php wp_link_pages(); ?>
 
-                    <?php wp_link_pages(); ?>
+					</div><!--. entry-->
 
-                </div><!--. entry-->
+					<footer class="post-footer">
 
-                <footer class="post-footer">
+						<div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
 
-                    <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
+					</footer><!--.post-footer-->
 
-                </footer><!--.post-footer-->
+				</div><!-- .post-->
 
-            </div><!-- .post-->
-
+			<?php endif; ?>
+			
         <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
 
             <nav class="navigation index">
@@ -47,13 +53,13 @@
                 <div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
 
             </nav><!--.navigation-->
-
+			
         <?php else : ?>
 
     <?php endif; ?>
 
-</div>
 
 <?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
